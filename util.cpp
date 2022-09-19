d4dbfff6cbc686dc8fbc3a1bc20266f904d1a2d3
rayxie11 <91774877+rayxie11@users.noreply.github.com>
2022-09-19 16:44:01 -0700

    initial upload

---
+++ util.cpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "util.h"
+
+#include <cmath>
+
+// Earth radius constant
+static const double EARTH_RADIUS = 6356.752;
+
+
+// Changes the angle from degree to radian
+double Util::toRadians(double degree){
+    double deg = M_PI/180.0;
+    return deg*degree;
+}
+
+// Returns the distance between two charging stations
+double Util::dist(row s1, row s2){
+    double lat1 = Util::toRadians(s1.lat);
+    double lon1 = Util::toRadians(s1.lon);
+    double lat2 = Util::toRadians(s2.lat);
+    double lon2 = Util::toRadians(s2.lon);
+
+    double diffLat = lat2-lat1;
+    double diffLon = lon2-lon1;
+
+    double ans = pow(sin(diffLat/2.0),2.0)+
+                     cos(lat1)*cos(lat2)*
+                     pow(sin(diffLon/2.0),2.0);
+    
+    return 2*asin(sqrt(ans))*EARTH_RADIUS;
+}
---
+++ util.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+#include "network.h"
+
+// Utility class containing different utility functions
+
+namespace Util{
+    double toRadians(double degree);
+    double dist(row s1, row s2);
+}
